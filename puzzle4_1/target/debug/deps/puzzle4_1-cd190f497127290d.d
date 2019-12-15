fec1fb5e1ad05c575fbf976d7c6ca90dec3ba823
Christian Griggs <christian.griggs@walmart.com>
2019-12-14 20:12:20 -0500

    Finished puz41:

--- puzzle4_1/target/debug/deps/puzzle4_1-cd190f497127290d.d
+++
@@ -1,5 +0,0 @@
-/Users/c0g0237/Work/adventofcode/puzzle4_1/target/debug/deps/puzzle4_1-cd190f497127290d: src/main.rs
-
-/Users/c0g0237/Work/adventofcode/puzzle4_1/target/debug/deps/puzzle4_1-cd190f497127290d.d: src/main.rs
-
-src/main.rs:
--- puzzle4_1/target/debug/puzzle4_1.d
+++
@@ -1,1 +0,0 @@
-/Users/c0g0237/Work/adventofcode/puzzle4_1/target/debug/puzzle4_1: /Users/c0g0237/Work/adventofcode/puzzle4_1/src/main.rs
